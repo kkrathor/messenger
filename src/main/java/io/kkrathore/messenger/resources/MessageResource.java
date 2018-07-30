7fa27d588e100c4c2e7ba05759a6eaa867d24d29
rathor-kk <rathor.kk@gmail.com>
2018-07-30 20:00:19 +0530

    Implement POST message to add a new Message using Postman

--- src/main/java/io/kkrathore/messenger/resources/MessageResource.java
+++ src/main/java/io/kkrathore/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package io.kkrathore.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,6 +23,13 @@ public class MessageResource {
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 		//return "Hello World in Messenger MessageResource";
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessages(Message message) {
+		return messageService.addMessage(message);
 	}
 	
 	@GET
