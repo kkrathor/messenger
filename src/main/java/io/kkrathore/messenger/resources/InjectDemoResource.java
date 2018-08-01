5fa6f6314351ff104a28da8ddb94d4b0a9ae9271
rathor-kk <rathor.kk@gmail.com>
2018-08-01 09:32:28 +0530

    Implement Context Param types HttpHeader and UriInfo

--- src/main/java/io/kkrathore/messenger/resources/InjectDemoResource.java
+++ src/main/java/io/kkrathore/messenger/resources/InjectDemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Produces(MediaType.TEXT_PLAIN)
@@ -15,11 +18,19 @@ import javax.ws.rs.core.MediaType;
 public class InjectDemoResource {
 
 	@GET
-	@Path("/annotation")
-	public String test(@MatrixParam("param") String matrixParam, 
+	@Path("annotation")
+	public String getParamsUsingAnnotation(@MatrixParam("param") String matrixParam, 
 			           @HeaderParam("header") String header,
 			           @CookieParam("JSESSIONID") String cookieValue) {
 		System.out.println("Test");
 		return "Test " + " \nMatrix Param  " + matrixParam + "\nHeader " + header + "\nCookieValue" + cookieValue;
+	}
+	
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo,
+										@Context HttpHeaders headers) {
+		return "Path : " + uriInfo.getAbsolutePath().toString() +
+				"\nCookies :" + headers.getCookies().toString();
 	}
 }
