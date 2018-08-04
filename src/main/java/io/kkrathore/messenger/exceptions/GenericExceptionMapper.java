79d8126bf202dd2ee1bb0d45337127171925f53f
rathor-kk <rathor.kk@gmail.com>
2018-08-04 16:29:58 +0530

    Implemented a Generic Exception Mapper which takes all the exceptions

---
+++ src/main/java/io/kkrathore/messenger/exceptions/GenericExceptionMapper.java
@@ -0,0 +1,21 @@
+package io.kkrathore.messenger.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import io.kkrathore.messenger.model.ErrorMessage;
+
+@Provider
+public class GenericExceptionMapper  implements ExceptionMapper<Throwable>{
+
+		@Override
+		public Response toResponse(Throwable exception) {
+			ErrorMessage errorMessage = new ErrorMessage(exception.getMessage(),500,"http://kkrathore.io");
+			return Response.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(errorMessage)
+					.build();
+		}
+
+	}
